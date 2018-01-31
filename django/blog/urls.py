f17836099276ea233b7b15838735945407849704
park chan <parkchan428@gmail.com>
2018-01-31 16:29:34 +0900

    파일 날려먹고 재시작

---
+++ django/blog/urls.py
@@ -0,0 +1,17 @@
+import re
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('list', views.post_list, name = 'post-list'),
+
+    # 3/
+    # 53/
+    # 53/asdf/ xx
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    # path('', views.post_detail),
+    # 숫자가 한개이상 반복되는 경우를 정규표현식을 구현하되
+    # 해당 반복구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
+    # re.compile(r'(?P<pk>\d+)')
+    ]
