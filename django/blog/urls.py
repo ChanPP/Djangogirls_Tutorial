8eb83876cd945d2d1d82d77723092d0517c50951
park chan <parkchan428@gmail.com>
2018-02-01 11:31:14 +0900

    post_add view, url, template생성 및 base.html에서 연결 링크 구현

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -3,14 +3,17 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('list', views.post_list, name='post-list'),
+    path('', views.post_list, name='post-list'),
 
     # 3/
     # 53/
     # 53/asdf/ xx
     # re_path(r'(?P<pk>\d+)/$', views.post_detail),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
+
     # path('', views.post_detail),
+
+    path('add/', views.post_add, name='post-add')
     # 숫자가 한개이상 반복되는 경우를 정규표현식을 구현하되
     # 해당 반복구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
     # re.compile(r'(?P<pk>\d+)')
